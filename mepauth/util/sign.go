12654ed9fe64213a58da5d6a470183e9c340e3e6
sunjinghan <hw_sunjh@163.com>
2020-10-19 20:08:33 +0800

    Title:修改校验签名的算法 https://[ip]:8443/mepauth/mepauth/v1/token一直提示Invalid account credential #I1UHDN:https://[ip]:8443/mepauth/mepauth/v1/token一直提示Invalid account credential
    Detail:参考https://support.huaweicloud.com/devg-apisign/api-sign-algorithm.html，修改签名加密算法

--- mepauth/util/sign.go
+++ mepauth/util/sign.go
@@ -18,11 +18,13 @@
 package util
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -77,12 +79,19 @@ func getCanonicalRequest(req *http.Request) (string, error) {
 	method := req.Method
 	// request uri
 	uri := getCanonicalUri(req)
+	// query string
+	query := getCanonicalQueryString(req)
 	// request headers
 	headersReq := getCanonicalHeaders(req)
 	// signed headers
 	headersSign := getSignedHeaders(req)
+	// request body
+	hexEncodeBody, errGetRequestBodyHash := getRequestBodyHash(req)
+	if errGetRequestBodyHash != nil {
+		return "", errGetRequestBodyHash
+	}
 	// construct complete
-	return strings.Join([]string{method, uri, headersReq, headersSign}, LINE_SEPARATOR), nil
+	return strings.Join([]string{method, uri, query, headersReq, headersSign, hexEncodeBody}, LINE_SEPARATOR), nil
 }
 
 // construct canonical uri can return
@@ -105,6 +114,20 @@ func getCanonicalUri(req *http.Request) string {
 	} else {
 		return canonicalUri + SEPARATOR
 	}
+}
+
+// construct canonical query string and return
+func getCanonicalQueryString(req *http.Request) string {
+
+	var params []string
+	for key, values := range req.URL.Query() {
+		for _, value := range values {
+			// canonical query string with each value
+			params = append(params, url.QueryEscape(key) + "=" + url.QueryEscape(value))
+		}
+	}
+	sort.Strings(params)
+	return strings.Join(params, "&")
 }
 
 // construct canonical request headers and return
@@ -134,6 +157,34 @@ func getSignedHeaders(req *http.Request) string {
 	}
 	sort.Strings(headers)
 	return strings.Join(headers, ";")
+}
+
+// get request body, do sha256 encrypt and hex encode
+func getRequestBodyHash(req *http.Request) (string, error) {
+
+	reqBody, errGetRequestBody := getRequestBody(req)
+	if errGetRequestBody != nil {
+		return "", errGetRequestBody
+	}
+	hexEncode, errHexEncode := hexEncodeSHA256Hash(reqBody)
+	if errHexEncode != nil {
+		return "", errHexEncode
+	}
+	return hexEncode, nil
+}
+
+// get request body bytes
+func getRequestBody(req *http.Request) ([]byte, error) {
+
+	if req.Body == nil {
+		return []byte(""), nil
+	}
+	body, errReadAll := ioutil.ReadAll(req.Body)
+	if errReadAll != nil {
+		return []byte(""), errReadAll
+	}
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	return body, nil
 }
 
 // HexEncode(Hash(bytes)) with SHA256
