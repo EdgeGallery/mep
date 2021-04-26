2af0451f04aff50a3b72ec4896fc85c0edadf563
鹿鑫 <luxin7@huawei.com>
2021-04-26 10:10:53 +0800

    Add beego framework code

--- healthcheck/go.mod
+++ healthcheck/go.mod
@@ -3,5 +3,14 @@ module health-check
 go 1.14
 
 require (
-
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/astaxie/beego v1.12.0
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ healthcheck/src/config/log.go
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+// logrus config
+package config
+
+import (
+	"github.com/natefinch/lumberjack"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	fileName := "/usr/mep/log/health-check.log"
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	if err == nil {
+		err = file.Close()
+		if err != nil {
+			logrus.Error("failed to close the log file")
+			return
+		}
+		ioWriter := &lumberjack.Logger{
+			Filename:   fileName,
+			MaxSize:    20,   // megabytes
+			MaxBackups: 50,   // max number of file backups
+			MaxAge:     30,   // days
+			Compress:   true, // compress
+		}
+		logrus.SetOutput(ioWriter)
+
+	} else {
+		logrus.Warn("Failed to log to file, using default stderr")
+	}
+
+	logrus.SetLevel(logrus.InfoLevel)
+}
